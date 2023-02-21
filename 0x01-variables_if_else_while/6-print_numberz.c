f258a91bbe427bdc9613e5086b599676db82aee6
Muhammad <Muhammadtijjani534@gmail.com>
2023-02-21 10:08:57 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 9 without using the 'char' variable.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
