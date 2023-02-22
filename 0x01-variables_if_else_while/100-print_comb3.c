c21e4d076433028acd66729091ed9a0e4d8dbb82
Muhammad <Muhammadtijjani534@gmail.com>
2023-02-22 03:11:56 -0800

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - It prints all possible combination of two numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
